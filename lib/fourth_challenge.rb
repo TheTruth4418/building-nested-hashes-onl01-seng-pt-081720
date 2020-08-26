e87d0a1e56e4eb64c2273034fe9e253e2d28393f
William West <wwest466@gmail.com>
2020-08-26 01:19:33 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -17,6 +17,7 @@ epic_tragedy = {
         :heroine_friends => [
             {name: "Steven", age: "30", attitude: "confused"},
             {name: "Nurse", age: "44", attitude: "worried"}
+          ]
  }
 }
 
